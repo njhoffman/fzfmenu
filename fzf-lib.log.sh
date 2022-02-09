a293c274528ec8f9bb28c07857827c562bc8f6f7
Nicholas Hoffman <njhoffman1982@gmail.com>
2022-02-09 12:32:27 -0600

    refactor and cleanup

--- fzf-lib.log.sh
+++ fzf-lib.log.sh
@@ -7,9 +7,17 @@ fi
 # signal logger to output file header
 _fzf_log_first=0
 _fzf-log() {
+  local clr_div="\\033[1m"
+  local clr_rst="\\033[0m"
+  local src="${SOURCE/$HOME/~}"
+
+  local divider="${clr_div}"
+  for _ in {1..40}; do divider="${divider}-"; done
+  divider="\n${divider}${clr_rst}\n${src}\n"
+
   if [[ -n ${FZF_LOGFILE} && ${FZF_LOGFILE} != 0 ]]; then
     [[ _fzf_log_first -eq 0 ]] \
-      && printf "--------------------\n${SOURCE}\n" >> ${FZF_LOGFILE}
+      && printf "$divider" >> ${FZF_LOGFILE}
     echo -e "\n$*" >> ${FZF_LOGFILE} && _fzf_log_first=1
   fi
 }
