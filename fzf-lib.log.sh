346f293ca3b2dfedf161bd717a2509ccbc9aca99
Nick Hoffman <njhoffman1982@gmail.com>
2024-08-20 06:01:59 -0500

    updates

--- fzf-lib.log.sh
+++ fzf-lib.log.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
 if [[ -n ${FZF_LOGFILE} && ${FZF_LOGFILE} != 0 ]]; then
-  [[ ! -f ${FZF_LOGFILE} ]] && touch ${FZF_LOGFILE}
+  [[ ! -f ${FZF_LOGFILE} ]] && touch "$FZF_LOGFILE"
 fi
 
 # signal logger to output file header
 _fzf_log_first=0
 _fzf-log() {
-  local clr_div="\\033[1m"
-  local clr_rst="\\033[0m"
+  local clr_div='\034[1m'
+  local clr_rst='\033[0m'
   local src="${SOURCE/$HOME/~}"
 
-  local divider="${clr_div}"
-  for _ in {1..40}; do divider="${divider}-"; done
+  local divider="$clr_div―"
+  for _ in {1..40}; do divider="$divider"; done
   divider="\n${divider}${clr_rst}\n${src}\n"
 
   if [[ -n ${FZF_LOGFILE} && ${FZF_LOGFILE} != 0 ]]; then
-    [[ _fzf_log_first -eq 0 ]] \
-      && printf "$divider" >> ${FZF_LOGFILE}
-    echo -en "\n$*" >> ${FZF_LOGFILE}
+    [[ _fzf_log_first -eq 0 ]] &&
+      printf "$divider" >> "$FZF_LOGFILE"
+    echo -en "\n$*" >> "$FZF_LOGFILE"
     _fzf_log_first=1
   fi
 }
